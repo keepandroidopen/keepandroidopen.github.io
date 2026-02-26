7240f433adec15bd898d9619b073ec3785df0f7e
Mehrab Mahmud Udoy <rimehrab@gmail.com>
2026-02-27 02:59:26 +0600

    Update config.ts

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -22,6 +22,7 @@ export const languages = {
   "zh-CN": { label: "简体中文", path: "/zh-CN/" },
   "zh-TW": { label: "正體中文", path: "/zh-TW/" },
   "ja": { label: "日本語", path: "/ja/" },
+  "bn": { label: "বাংলা", path: "/bn/" },
 } as const;
 
 export type Locale = keyof typeof languages;
